77a6abafce615e264da8943b79d0a59a38b25214
ghiscoding <gbeaulac@gmail.com>
2025-04-26 20:08:45 -0400

    chore(deps): update Slickgrid-React to v5.14.0

--- bootstrap5-i18n-demo/src/examples/slickgrid/Example33.tsx
+++ bootstrap5-i18n-demo/src/examples/slickgrid/Example33.tsx
@@ -21,7 +21,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import './example33.scss';
 
 const FAKE_SERVER_DELAY = 500;
-const NB_ITEMS = 500;
+const NB_ITEMS = 1000;
 
 const Example33: React.FC = () => {
   const [columnDefinitions, setColumnDefinitions] = useState<Column[]>([]);
@@ -30,6 +30,7 @@ const Example33: React.FC = () => {
   const [serverWaitDelay, setServerWaitDelay] = useState<number>(FAKE_SERVER_DELAY);
   const [editCommandQueue] = useState<EditCommand[]>([]);
   const [hideSubTitle, setHideSubTitle] = useState(false);
+  const [showLazyLoading, setShowLazyLoading] = useState(false);
 
   const serverWaitDelayRef = useRef(serverWaitDelay);
   const reactGridRef = useRef<SlickgridReactInstance | null>(null);
@@ -220,7 +221,7 @@ const Example33: React.FC = () => {
         type: FieldType.string,
         editor: {
           // OR 1- use `fetch`, Promise or RxJS when available
-          // collectionAsync: fetch(URL_SAMPLE_COLLECTION_DATA),
+          // collectionAsync: fetch(SAMPLE_COLLECTION_DATA_URL),
 
           // OR 2- use a Promise
           collectionAsync: new Promise<any>((resolve) => {
@@ -239,19 +240,33 @@ const Example33: React.FC = () => {
           model: Editors.multipleSelect,
         },
         filter: {
-          // collectionAsync: fetch(URL_SAMPLE_COLLECTION_DATA),
-          collectionAsync: new Promise((resolve) => {
-            window.setTimeout(() => {
-              resolve(Array.from(Array(dataset?.length).keys()).map(k => ({ value: k, label: `Task ${k}` })));
+          // collectionAsync: fetch(SAMPLE_COLLECTION_DATA_URL),
+          // collectionAsync: new Promise((resolve) => {
+          //   window.setTimeout(() => {
+          //     resolve(Array.from(Array(dataset.value?.length).keys()).map((k) => ({ value: k, label: `Task ${k}` })));
+          //   });
+          // }),
+          collectionLazy: () => {
+            setShowLazyLoading(true);
+
+            return new Promise((resolve) => {
+              window.setTimeout(() => {
+                setShowLazyLoading(false);
+                resolve(Array.from(Array((dataset || []).length).keys()).map((k) => ({ value: k, label: `Task ${k}` })));
+              }, serverWaitDelayRef.current);
             });
-          }),
+          },
+          // onInstantiated: (msSelect) => console.log('ms-select instance', msSelect),
           customStructure: {
             label: 'label',
             value: 'value',
             labelPrefix: 'prefix',
           },
           collectionOptions: {
-            separatorBetweenTextLabels: ' '
+            separatorBetweenTextLabels: ' ',
+          },
+          filterOptions: {
+            minHeight: 70,
           },
           model: Filters.multipleSelect,
           operator: OperatorType.inContains,
@@ -519,13 +534,17 @@ const Example33: React.FC = () => {
         </ul>
       </div>
 
-
-      <div style={{ marginBottom: '20px' }}>
-        <label htmlFor="pinned-rows">Simulated Server Delay (ms): </label>
-        <input type="number" id="server-delay" data-test="server-delay" style={{ width: '60px' }}
-          value={serverWaitDelay}
-          onInput={($event) => handleServerDelayInputChange($event)}
-        />
+      <div className="row">
+        <div className="col" style={{ marginBottom: '20px' }}>
+          <label htmlFor="pinned-rows">Simulated Server Delay (ms): </label>
+          <input type="number" id="server-delay" data-test="server-delay" style={{ width: '60px' }}
+            value={serverWaitDelay}
+            onInput={($event) => handleServerDelayInputChange($event)}
+          />
+        </div>
+        <div className={`alert alert-info is-narrow col ${!showLazyLoading ? 'invisible' : ''}`} data-test="alert-lazy">
+          Lazy loading collection...
+        </div>
       </div>
 
       <div id="smaller-container" style={{ width: '950px' }}>
