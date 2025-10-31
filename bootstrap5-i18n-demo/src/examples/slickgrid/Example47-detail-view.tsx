55f04571b599bf5547e48a1158ea006a7ada3574
ghiscoding <gbeaulac@gmail.com>
2025-10-30 23:53:04 -0400

    chore: add btn-icon to align icon

--- bootstrap5-i18n-demo/src/examples/slickgrid/Example47-detail-view.tsx
+++ bootstrap5-i18n-demo/src/examples/slickgrid/Example47-detail-view.tsx
@@ -114,7 +114,7 @@ const Example47DetailView: React.FC<RowDetailViewProps<Item, any>> = forwardRef(
           Delete Row
         </button>
         <button
-          className="btn btn-outline-secondary btn-sm"
+          className="btn btn-outline-secondary btn-sm btn-icon"
           onClick={function () {
             showNotification(props.model);
           }}
