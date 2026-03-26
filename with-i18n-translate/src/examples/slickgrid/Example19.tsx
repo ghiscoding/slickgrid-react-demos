82d931ea90e1bbf8309bec38a7c14904daa96e45
ghiscoding <gbeaulac@gmail.com>
2026-03-26 00:16:39 -0400

    chore: improve Row Detail demos

--- with-i18n-translate/src/examples/slickgrid/Example19.tsx
+++ with-i18n-translate/src/examples/slickgrid/Example19.tsx
@@ -50,8 +50,6 @@ const Example19: React.FC = () => {
         id: 'duration',
         name: 'Duration (days)',
         field: 'duration',
-        formatter: Formatters.decimal,
-        params: { minDecimal: 1, maxDecimal: 2 },
         sortable: true,
         type: 'number',
         minWidth: 90,
@@ -200,7 +198,7 @@ const Example19: React.FC = () => {
       tmpData[i] = {
         rowId: i,
         title: 'Task ' + i,
-        duration: i % 33 === 0 ? null : Math.random() * 100 + '',
+        duration: i % 33 === 0 ? null : Math.floor(Math.random() * 100) + 1,
         percentComplete: randomPercent,
         percentComplete2: randomPercent,
         percentCompleteNumber: randomPercent,
